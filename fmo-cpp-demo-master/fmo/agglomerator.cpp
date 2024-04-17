d8aaef90a5cb85bd69ce6d365f05efb1c0b98cad
mfortelnyy <mfortelnyy@hawk.iit.edu>
2024-04-17 15:08:02 -0500

    upd

--- fmo-cpp-demo-master/fmo/agglomerator.cpp
+++
@@ -1,5 +0,0 @@
-#include <fmo/agglomerator.hpp>
-
-namespace fmo {
-    constexpr Agglomerator::Id_t Agglomerator::safetyMaxNumClusters;
-}
--- fmo-cpp-demo-master/tests/benchmark-main.cpp
+++
@@ -1,7 +0,0 @@
-#include <fmo/benchmark.hpp>
-#include <iostream>
-
-int main() {
-    fmo::Registry::get().runAll([](const char* cStr) { std::cout << cStr; },
-                                []() { return false; });
-}
